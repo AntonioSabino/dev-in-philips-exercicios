399c8ec811a90d6d6d9efeec706259532f83f189
Antonio Sabino <aslimajr@outlook.com>
2022-10-29 16:43:31 -0300

    Implementação do metodo de consulta de saldo

--- script.js
+++ script.js
@@ -7,13 +7,16 @@ function getData() {
     email: document.getElementById("email").value,
     celular: document.getElementById("phone").value,
     senha: document.getElementById("password").value,
-    conta: Math.floor(Math.random() * 1000000000),
+    conta: Math.floor(Math.random() * 1000000000).toString(),
+    saldo: 0,
   };
 }
 
 function deposit() {}
 
-function getBalance() {}
+function getBalance(account) {
+  alert(`Seu saldo é de R$ ${account.saldo}`);
+}
 
 function withdraw() {}
 
@@ -26,7 +29,6 @@ form.addEventListener("submit", (e) => {
   }
   const newAccount = getData();
   accounts.push(newAccount);
-  console.log(accounts);
   form.reset();
   alert(`Conta criada com sucesso. Número da conta: ${newAccount.conta}`);
 });
@@ -34,20 +36,27 @@ form.addEventListener("submit", (e) => {
 const select = document.querySelector("select");
 select.addEventListener("change", (e) => {
   const value = document.getElementById("value");
-  operation === "balance" ? (value.disabled = true) : (value.disabled = false);
+  e.target.value === "balance"
+    ? value.setAttribute("disabled", "")
+    : value.removeAttribute("disabled");
 });
 
 const operationForm = document.querySelector(".operation-form");
 operationForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const data = getData();
-  const account = accounts.find((account) => account.conta === data.conta);
+  const account = accounts.find((a) => a.conta === e.target.account.value);
 
-  if (!account) alert("Conta não encontrada");
-  if (account.senha !== data.senha) alert("Senha incorreta");
+  if (!account) {
+    alert("Conta não encontrada");
+    return;
+  }
+  if (account.senha !== e.target.password.value) {
+    alert("Senha incorreta");
+    return;
+  }
 
-  const operation = select.value;
+  const operation = e.target.operation.value;
   if (operation === "deposit") deposit();
-  if (operation === "balance") getBalance();
+  if (operation === "balance") getBalance(account);
   if (operation === "withdraw") withdraw();
 });
