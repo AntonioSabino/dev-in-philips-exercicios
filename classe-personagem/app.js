899c475fa08e5697ffa9f2f58b034f9e530960bb
Antonio Sabino <aslimajr@outlook.com>
2022-11-05 12:15:03 -0300

    Classe Personagem

---
+++ classe-personagem/app.js
@@ -0,0 +1,13 @@
+import Personagem from "./Personagem.js";
+
+const alyx = new Personagem("Alyx");
+
+console.log(alyx.percentualVida);
+
+alyx.sofreuDano(50);
+
+console.log(alyx.percentualVida);
+
+alyx.usouKitMedico();
+
+console.log(alyx.percentualVida);
