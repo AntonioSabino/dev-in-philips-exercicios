7bda340163612e4ddd58d1066e7a723d93c75f46
Antonio Sabino <aslimajr@outlook.com>
2022-11-11 19:57:27 -0300

    Resolução PromisseRace

---
+++ PromisseRace/promisseRace.js
@@ -0,0 +1,13 @@
+const retornoEmpresa1 = new Promise((resolve, reject) =>
+  setTimeout(resolve, 5000, "Empresa 1")
+); // 5 segundos
+const retornoEmpresa2 = new Promise((resolve, reject) =>
+  setTimeout(resolve, 3000, "Empresa 2")
+); // 3 segundos
+
+const venda = Promise.race([retornoEmpresa1, retornoEmpresa2])
+
+console.log("A venda mais rápida é da...");
+venda.then((result) => {
+  console.log(result);
+});
