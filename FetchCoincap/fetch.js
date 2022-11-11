a12c938e068f3e5fae305a86dc765e7fe77ff183
Antonio Sabino <aslimajr@outlook.com>
2022-11-11 20:20:36 -0300

    Melhoria FetchCoincap

--- FetchCoincap/fetch.js
+++ FetchCoincap/fetch.js
@@ -1,8 +1,11 @@
-const getPrice = async () => {
-  const URL = 'https://api.coincap.io/v2/assets/bitcoin';
+const getCoinPrice = async (coin) => {
+  const URL = `https://api.coincap.io/v2/assets/${coin}`;
   const response = await fetch(URL);
-  const { data: { priceUsd, symbol } } = await response.json();
-  console.log(`O preço do Bitcoin - ${symbol} em dólares é $${Number(priceUsd).toFixed(2)}`);
+  const { data: { name, priceUsd, symbol } } = await response.json();
+  console.log(`O preço do ${name} - ${symbol} em dólares é $${Number(priceUsd).toFixed(2)}`);
 }
 
-getPrice();
+getCoinPrice("bitcoin");
+getCoinPrice("ethereum");
+getCoinPrice("litecoin");
+getCoinPrice("solana");
