62957062aa9de714a1df8e9a79b05fc1ad71d4c0
Antonio Sabino <aslimajr@outlook.com>
2022-11-24 19:22:07 -0300

    resolução requisito 9 e 10

--- notification-app/src/app/notification-list/notification-list.component.ts
+++ notification-app/src/app/notification-list/notification-list.component.ts
@@ -3,34 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ntap-notification-list',
   templateUrl: './notification-list.component.html',
-  styleUrls: ['./notification-list.component.scss']
+  styleUrls: ['./notification-list.component.scss'],
 })
 export class NotificationListComponent {
   notifications = [
     {
       title: 'Notification 1',
       subtitle: 'This is the first notification',
-      date: '24/11/2022'
+      date: '24/11/2022',
+      read: false,
     },
     {
       title: 'Notification 2',
       subtitle: 'This is the second notification',
-      date: '24/11/2022'
+      date: '24/11/2022',
+      read: false,
     },
     {
       title: 'Notification 3',
       subtitle: 'This is the third notification',
-      date: '24/11/2022'
+      date: '24/11/2022',
+      read: false,
     },
     {
       title: 'Notification 4',
       subtitle: 'This is the fourth notification',
-      date: '24/11/2022'
+      date: '24/11/2022',
+      read: false,
     },
     {
       title: 'Notification 5',
       subtitle: 'This is the fifth notification',
-      date: '24/11/2022'
+      date: '24/11/2022',
+      read: false,
     },
   ];
+
+  makeRead(notification: { read: boolean; }) {
+    notification.read = true;
+  }
 }
