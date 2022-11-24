1ec61917520f5c15b2f00e1da0eb0b5d2db490eb
Antonio Sabino <aslimajr@outlook.com>
2022-11-24 15:35:36 -0300

    resolução requisito 6

--- notification-app/src/app/app.module.ts
+++ notification-app/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NotificationListComponent } from './notification-list/notification-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NotificationListComponent],
+  declarations: [AppComponent, NotificationListComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
