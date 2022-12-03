bf1a11176869fa2b5e8e799e8acc35b42e0644fc
Antonio Sabino <aslimajr@outlook.com>
2022-12-03 11:45:34 -0300

    Ex 2 - inclusão do modal de sucesso

--- exercicios-semana9/src/app/app.module.ts
+++ exercicios-semana9/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
