5b9057124495f200d8d5d1a515ca8876ee46c7a5
Antonio Sabino <aslimajr@outlook.com>
2022-12-04 22:20:15 -0300

    Ex 6 - passando atributos do componente pai para o filho

--- exercicios-semana9/src/app/app.component.ts
+++ exercicios-semana9/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'exercicios-semana9';
+
+  @Input() personagem = {
+    nome: 'Dwight Kurt Schrute III',
+    imagem:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeKAZ1BkYu4kTpTAdspz_jVMIhE0poWHQNg-Xu4qxqpk82WLHh-mrJJslEksENVIknN3c&usqp=CAU',
+    nomeAtor: 'Rainn Wilson',
+    descricao:
+      'Dwight Kurt Schrute III é um personagem fictício em The Office e é interpretado pelo ator americano Rainn Wilson. O personagem de Dwight era um vendedor e assistente do gerente regional, na empresa fictícia de distribuição de papel, Dunder Mifflin, antes de suas promoções em temporadas posteriores.',
+  };
 }
--- exercicios-semana9/src/app/components/item-card/item-card.component.ts
+++ exercicios-semana9/src/app/components/item-card/item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-card',
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-card.component.css'],
 })
 export class ItemCardComponent {
-  personagem = {
-    nome: 'Dwight Kurt Schrute III',
-    imagem:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeKAZ1BkYu4kTpTAdspz_jVMIhE0poWHQNg-Xu4qxqpk82WLHh-mrJJslEksENVIknN3c&usqp=CAU',
-    nomeAtor: 'Rainn Wilson',
-    descricao:
-      'Dwight Kurt Schrute III é um personagem fictício em The Office e é interpretado pelo ator americano Rainn Wilson. O personagem de Dwight era um vendedor e assistente do gerente regional, na empresa fictícia de distribuição de papel, Dunder Mifflin, antes de suas promoções em temporadas posteriores.',
-  };
+  @Input() personagem: any;
 }
